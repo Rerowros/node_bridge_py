5223c2994e9dff6b13d820f4b26d64d843733158
Mohammad <immohammad20000@protonmail.com>
2026-09-01 15:25:45 +0330

    bump to v0.9.1

--- PasarGuardNodeBridge/__init__.py
+++ PasarGuardNodeBridge/__init__.py
@@ -12,10 +12,10 @@ Features:
 - Extensible with custom metadata via the `extra` argument
 
 Author: PasarGuard
-Version: 0.9.0
+Version: 0.9.1
 """
 
-__version__ = "0.9.0"
+__version__ = "0.9.1"
 __author__ = "PasarGuard"
 
 
